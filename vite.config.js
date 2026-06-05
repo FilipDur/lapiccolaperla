8ea9301b203e1c122dc0df4640f474131e7c35e8
FilipDur <filiposduricic24@gmail.com>
2026-06-05 18:09:14 +0200

    Update

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        en: resolve(__dirname, "en.html"),
+        it: resolve(__dirname, "it.html")
+      }
+    }
+  }
+});

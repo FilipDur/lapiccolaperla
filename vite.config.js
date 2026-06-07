c353cfa13b300072b3b8c676cf36d799266665eb
FilipDur <filiposduricic24@gmail.com>
2026-06-07 23:57:24 +0200

    Update configurations and add admin.html

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin.html"),
         en: resolve(__dirname, "en.html"),
         it: resolve(__dirname, "it.html")
       }
